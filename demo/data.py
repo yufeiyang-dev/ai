41ae10a36c502cece4573741450e5c899914462f
Yufei Yang <stevenwaterloo2015@gmail.com>
2025-01-10 17:02:57 -0500

    add demo materials

---
+++ demo/data.py
@@ -0,0 +1,235 @@
+delimiter = "```"
+
+vacation_policy = f"""
+Thoughtworks Canada Annual Vacation and Paid Time Off Policy
+
+
+Effective: January 1, 2023
+
+The goal of this policy is to explain to Thoughtworkers our paid time off program, including our Annual Vacation Policy, our Public Holiday schedule,  Personal/Sick days, our Service/Action Day and our Personal Development Days. 
+
+
+In this document:
+Who does this policy apply to?
+How to use this policy
+Policy Statement
+Annual Vacation
+Public Holidays 
+Personal/Sick Days
+Service/Action Day
+Personal Development Days 
+Roles and responsibilities
+Applicable standards and exceptions
+Change and legal effect
+Non-compliance
+Document history 
+Approvals
+
+
+Who does this policy apply to?
+This policy applies to all indefinite-hire Canadian employees, both full-time and part-time. This policy does not apply to interns, temporary employees, contractors, agency workers or Thoughtworkers on a long term assignment to Canada from another Thoughtworks country (“LTAs”).  LTAs should reference their assignment letters and home country policies for leave information and questions may be directed to Global Mobility. Interns, temporary employees, agency workers and contractors should reference their individual offer letters or contracts. 	
+
+
+How to use this policy
+This policy explains how your Annual Vacation Leave works , details which Public Holidays are paid days off,  how to use Personal/Sick days, your Service/Action Day and your Personal Development Days.  If you have additional questions the policy doesn’t answer, please reach out to hr@connected.io for additional help or information. 
+
+
+Policy Statement
+We recognize that the type of work we do at Thoughtworks can be fast paced, stressful, and challenging. The guiding principles of our Annual Vacation and Paid Time Off Policy are:  
+To provide Thoughtworkers with paid time to take a break from work, to refresh and recharge, to focus on their personal priorities (family, hobbies, life, etc.), and to prioritize their health.
+To create a working environment where Thoughtworkers can take this time off, allowing us to bring our best professional selves to work on a regular basis.
+To responsibly balance the success of our clients, the financial health of our company, and the wellbeing of our Thoughtworkers.
+
+While there may be times when it is difficult to impossible for people to take vacation, these occurrences should be few, short lived and far between.  No one should feel that they cannot take vacation for long periods of time…in fact, recharging is so important that if we feel it is necessary, Thoughtworks may schedule you to use some of your annual vacation.  Vacation leave is an entitlement you earn every year and you need to use it every year to achieve the balance you need to perform in your roles both in and outside of work.
+
+Our policies are intended to comply with employment/labour standards legislation (“Employment Standards”), human rights, equal opportunity and accommodation legislation, and other legislation that governs your employment rights in your province (“Applicable Legislation”). If ever Applicable Legislation mandates us to provide you with more favourable entitlements than those described in this policy, Thoughtworks will honour the legislative requirements.  Please reach out to hr@connected.io if you have questions.
+
+Annual Vacation
+
+Vacation Entitlement
+Vacation is allocated to Thoughtworkers annually based on the calendar year (our vacation “reference year”).  The total amount of annual vacation leave each employee is eligible to receive is based on their tenure with the company and Employment Standards rules. The amount of annual vacation leave you accrue during a given year is based on your hire date.
+All full-time employees will accrue annual vacation leave according to the following schedule: 
+Year 1-3: 15 days 
+After 3 completed years: 20 days 
+After 5 completed years: 25 days 
+If you are employed part time (not in Québec), your annual vacation accrual will be prorated in relation to the full-time schedule above, based on your actual work schedule. If you primarily work in Québec and are employed part time, your annual vacation time off will accrue based on the same full-time schedule above, but your annual vacation pay will be prorated based on your vacationable earnings.
+Your annual vacation is earned or accrued during the year. Every day you ‘accrue’ or earn a percentage of your total annual vacation. As you use your vacation, your accrued total vacation balance, including any vacation you were permitted to carry over from the previous year (see below), is reduced by the amount of vacation you have used.   
+
+Minimum Vacation Usage Expectation
+All employees are expected to take at least 2/3rds of their allocated vacation entitlement (annual vacation entitlement + any carryover from the previous year) in any given year in order to ensure they are taking the proper rest and recuperation time required to maintain a healthy work/life balance. To use your annual vacation to most effectively achieve this goal, employees should: 
+Take at least one instance of 5 days off in a row, not including weekends or holidays, during the year;
+Take a week off as soon as you hit the beach, especially after a challenging project; and/or
+Take off one day a week while you are on the beach to prioritize non-work activities - spend time with family, volunteer, or look after yourself in whatever way you need.
+
+Maximum Carryover 
+You are allowed to carry over a maximum of 10 unused accrued vacation days into the next calendar year and those carryover days must be used up by October 31st of the next calendar year.  
+Unused accrued vacation that exceeds the carry over limit and unused carry over days that are not used by the October 31st deadline will be lost and will not be paid out, except in each case to the extent strictly required to meet Applicable Legislative requirements..  
+
+Reduced Accruals
+There are times when your annual vacation accrual is reduced. This will result in you accruing less than your normal annual vacation time and/or pay entitlement in that year. There are 3 scenarios where a Thoughtworker’s annual vacation accrual is reduced:
+While on paid sabbatical leave, instead of your regular accrual, vacation time off and vacation pay entitlements will revert to the mandatory minimum entitlements under Applicable Legislation.  Your regular accrual will resume when you return from sabbatical. 
+During any other periods of extended absence that lasts longer than 4 continuous weeks (except vacation), instead of your regular accrual, vacation time off and vacation pay entitlements will revert to the mandatory minimum entitlements under Applicable Legislation for the full duration of that leave.  Your regular accrual will resume when you return from the leave.
+In addition, your annual accrual rate will be affected if you switch from a full-time schedule to a reduced work or part-time schedule (for example, as part of a disability accommodation that requires you to work a reduced work schedule for a few months).  In that situation, your entitlements will be based on your part-time schedule (see above) for as long as that part-time schedule is in effect.  Your regular accrual will resume if/when you resume full-time work. 	
+ 
+Advances on Annual Vacation Accrual (Borrowing Vacation Time)
+Thoughtworks allows advances on vacation accrual up to a maximum equivalent of 5 days (40 hours).  Approvals for advances of vacation leave must be obtained in advance of the requested leave and are up to the discretion of your Manager/Project Manager/CLT and People Support. In the event of any advance of vacation accrual, if your employment ends for any reason whatsoever prior to accrual of the advanced vacation, the unearned balance will be recoverable.  The recoverable balance will be deducted from your final paycheck to the extent permissible under Employment Standards, and any remaining balance will be repayable to the company by you.  
+
+Pay Out of Annual Vacation
+Annual vacation is only paid out to a Thoughtworker if their employment terminates for any reason .  If employment terminates, employees are paid out their accrued annual vacation balance as of their date of termination.  The employee’s accrued leave balance is multiplied by a calculated daily rate based on the Thoughtworker’s then current annual salary. 
+ 
+The resulting dollar amount could be positive or negative depending on how much annual vacation the Thoughtworker has borrowed.  The final amount (positive or negative) will be used to calculate the amount of your final paycheck.
+
+Public Holidays
+
+Thoughtworks provides the following paid holidays to eligible employees:
+New Year’s Day
+Family Day
+Good Friday
+Victoria Day
+Canada Day
+August Civic Holiday (rest of Canada)/Heritage Day (Alberta)*
+Labour Day
+Thanksgiving Day
+Remembrance Day*
+Christmas Eve*
+Christmas Day
+Boxing Day
+
+
+In order to ensure consistency across our Canadian operations, we will substitute certain Ontario holidays for those that may be customary in your Province of Work.
+
+* While not a public holiday under the Employment Standards Act of Ontario, you will be entitled to a day off with pay for the Civic Holiday in August, Remembrance Day in November, and Christmas Eve in December.
+
+Personal/Sick Days
+Personal/Sick days are provided by the company in acknowledgement of the unexpected need for employees to be away from work to care for themselves, their dependents, or to attend certain appointments. The company expects that employees will allocate their Personal/Sick days to absences for these types of purposes: 
+
+
+The employee’s medical care, preventative care, diagnosis, or treatment.
+The medical care, preventative care, diagnosis, or treatment of the employee’s family members (as defined below). 
+When an employee needs time away from work in response to certain personal situations including:
+
+
+For any employee who is a victim of domestic violence, sexual assault, or stalking to (1) obtain any relief or help to ensure the health, safety or welfare of the employee or the employee’s child (such as restraining orders or other injunctive relief); (2) seek medical attention; (3) obtain services from a shelter, program, rape crisis center, or victim services organization or agency; (4) obtain psychological counseling or mental health services; or (5) participate in safety planning or take other actions to increase safety from future incidents. 
+To attend judicial proceedings related to a serious violent crime or crime involving theft or embezzlement when the employee or the employee’s family member is a victim of the crime.
+
+
+For the purposes of this policy, “family member” means a child, spouse, domestic partner, parent, grandparent, grandchild, and sibling. A “child” includes a biological, adopted, or foster child, step child, legal ward, or a child to whom the employee stands in place of a parent. A “parent” includes a biological, adoptive, or foster parent, stepparent, or legal guardian of any employee or the employee’s spouse or domestic partner. 
+The total maximum number of paid Personal/Sick days provided to each employee per calendar year is seven (7). 
+
+
+Personal/Sick days are prorated for employees during their first calendar year of employment based on their start date, and prorated for interns or temporary employees based on the expected duration of their employment (see offer letter or contract for details). See below for the breakdown of proration based on hire date for full-time employees.
+
+
+Personal/Sick Days Allowed Per Year
+Start Date on or Between         1/1 - 4/30    5/1 - 8/31    9/1 - 12/31
+Full-time - Personal/Sick Days    7        	4        		3
+
+Personal/Sick days carryover from one calendar year to the next, however, the maximum number of Personal/Sick days you may take during a given calendar year is 7.  Personal/Sick days cannot be advanced or borrowed and will not be paid out upon termination of employment.
+
+Personal/Sick days should not be confused with annual vacation and should not be used to extend or to take vacations. 
+When you use Personal/Sick days for any purpose also covered by any Applicable Legislation, the paid days will be credited toward the Applicable Legislative leave entitlements.
+
+Service/Action Day 
+As a commitment to driving positive social change, Thoughtworks provides employees with one (1) paid Service/Action Day which can be used by employees to offer their talents or their voices in support of organizations working to build a more equitable and just world. Employees should use their Service/Action Day to make an impact and improve the world around them through contributions they would not otherwise have been able to make without specific time off to do so. Employees may consider any of the following: 
+
+
+Volunteer to work the polls or do political canvassing
+Attend peaceful protests against racial violence or similar social justice causes
+March for causes - Climate Change, Women’s Rights, Immigration Reform
+Volunteer to prepare and serve meals or deliver food to those that are food insecure
+
+Personal Development Days 
+Developing your career means something different for all of us and while ThoughtWorks provides internal learning and training opportunities, it may not always meet your specific needs. In connection with Thoughtworks’ Personal Development Policy, eligible full-time employees may take two (2) Personal Development Days to enhance their careers. No approval is needed in terms of how you use your Personal Development Days, however, time off must be requested and approved through your manager or your project/team leadership if you are staffed.
+
+
+Roles and responsibilities
+
+Role: Thoughtworker
+
+Description: 
+Schedule your leave with as much advance notice as possible.  
+Rule of thumb - the advance notice should be at least a week or twice the duration of the time off you are requesting, whichever is longer.
+
+Responsibilities: 
+Request time off in advance.
+Get approval.
+Ensure that the appropriate systems/calendars are updated.
+Provide handover information to your team so they can continue working while you are out.
+Fill out your timesheet to reflect you are on leave.
+
+Role: CLT Member or Staffing for beach folks(Professional Services) OR Functional Manager (Operations)
+
+Description: 
+Review and approve time off requests in a timely manner.
+Denying time off should be a rare occurrence.  
+If you need to decline time off, provide an alternative option(s) for the employee to take their leave. 
+(Always Consult HR first if the employee is asking for a statutory leave – entitlement to that may be automatic if the employee qualifies under Applicable Laws.)
+
+Responsibilities: 
+Work with Thoughtworkers to ensure they are allocated time to take leave.
+Ensure Thoughtworkers are not asked to work while on leave.
+Manage the overall leave schedule for the team so we know when folks are out and when they are working.
+
+
+
+Applicable standards and exceptions
+Types of Employees
+Hourly paid employees are paid an hourly rate for each hour worked. They are expected to report work time for the schedule agreed with the employee. Hourly employees are required to use and report paid Personal/Sick days if they are unavailable to work as scheduled for reasons covered by the Personal/Sick day policy. 
+
+Salaried employees receive a salary for a body of work, which is benchmarked at 40 hours per week. Full time employees are expected to report full days of work time or (as applicable) paid time off Monday through Friday, even if they reach 40 hours without reporting all five work days. Time spent traveling to and from the project site is to be recorded as non-billable time over and above the 40-hour schedule.  Every effort should be made to bill 40 hours each week.
+
+Exceptions to the 40 billable hours guideline include:
+Prior approval from project manager/department manager (please include a note on your timesheet)
+Working a pre-approved alternative work schedule 
+Absence due to any form of statutory leave for which you are eligible under Applicable Laws (where company approval is not required)
+
+
+Full-time salaried employees who work less than eight (8) hours each day are required to use and report paid Personal/Sick days if they are unavailable to work as scheduled for reasons covered by the Personal/Sick day policy
+
+
+Questions not answered in this policy or in the FAQ document may be emailed to hr@connected.io. 
+
+
+Timesheet Errors
+If you make an error in completing your timesheet, you may request a correction by emailing timesheet-support@Thoughtworks.com. 
+
+
+Change and legal effect
+Thoughtworks will review this policy from time to time to ensure it complies with  Applicable Legislature . A review is also required when significant changes are made to Thoughtworks’ business model or information systems technology. We reserve the right to change this policy at our discretion, subject always to compliance with Applicable Legislation.
+
+
+Non-compliance
+Failure to comply with this policy may lead to disciplinary measures, to the maximum extent permitted by local laws.
+
+
+Document history 
+Version
+Description
+Policy Owner
+Effective Date
+1.0
+First released version
+People Support
+2022
+
+
+
+Approvals
+Version
+Approval Date
+Approved by
+1.0
+10 Nov 2022
+Valarie Fairchild, North America Legal Counsel
+"""
+
+step_2_system_message_content = f"""
+You will be provided with Thoughtwork Canada's Vacation Policy \
+The most recent user query will be delimited with \
+{delimiter} characters.
+"""
+
+step_2_system_message = [ {
+    'role': 'system',
+    'content': step_2_system_message_content
+} ]
